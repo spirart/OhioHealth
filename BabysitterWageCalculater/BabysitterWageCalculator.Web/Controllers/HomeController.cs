043454230270c8ea5ab3bfab18e98f20c408a6ef
Shannon Coleman <shannon.coleman01@gmail.com>
2018-10-15 04:46:03 -0400

    Adding Ajax call to API

---
+++ BabysitterWageCalculater/BabysitterWageCalculator.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using BabysitterWageCalculator.Web.Models;
+namespace BabysitterWageCalculator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(Models.BabysitterWageCalculator babysitterWageCalculator)
+        {
+            return View("Index", babysitterWageCalculator);
+        }
+    }
+}
---
+++ BabysitterWageCalculater/BabysitterWageCalculator.Web/Models/BabysitterWageCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BabysitterWageCalculator.Web.Models
+{
+    public class BabysitterWageCalculator
+    {
+        private DateTime _startTime = DateTime.MinValue;
+        private DateTime _endTime = DateTime.MinValue;
+        private DateTime _bedTime = DateTime.MinValue;
+        private Decimal _totalWage = 0;
+
+        public BabysitterWageCalculator()
+        {
+        }
+
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public DateTime bedTime { get; set; }
+        public Decimal totalWage { get; set; }
+    }
+}
