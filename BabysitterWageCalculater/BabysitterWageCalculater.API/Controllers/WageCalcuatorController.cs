2a4b76a6973944f9ada70ef99577a0785219fc68
Shannon Coleman <shannon.coleman01@gmail.com>
2018-10-15 01:07:11 -0400

    Adding an asp.NEt Web API

---
+++ BabysitterWageCalculater/BabysitterWageCalculater.API/Controllers/WageCalcuatorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BabysitterWageCalculater.API.Models;
+
+namespace BabysitterWageCalculater.API.Controllers
+{
+    public class WageCalcuatorController : ApiController
+    {
+        [Route("api/WageCalcuator/GetBabysitterWage/{startTime:DateTime}/{beginTime:DateTime}/{bedTime:DateTime}")]
+        [HttpPost]
+        public decimal GetBabysitterWage(DateTime startTime, DateTime endTime, DateTime bedTime)
+        {
+            TimeSpan timeDifference = endTime.Subtract(startTime);
+            long hourDifference = timeDifference.Hours;
+            DateTime hourWorked = DateTime.MinValue;
+            decimal hourlyWagesSum = 0;
+            List<Models.HourlyRate> hourlyRates = new List<HourlyRate>();
+
+            for (int i = 0; i <= hourDifference; ++i)
+            {
+                var hourlyRate = new HourlyRate( startTime.AddHours(i), bedTime);
+                hourlyRates.Add(hourlyRate);
+            }
+
+            hourlyWagesSum = hourlyRates.Select(x => x.wage).Sum();
+
+            return hourlyWagesSum;
+        }     
+    }
+}
---
+++ BabysitterWageCalculater/BabysitterWageCalculater.API/Models/HourlyRate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BabysitterWageCalculater.API.Models
+{
+    public class HourlyRate
+
+    {
+        private DateTime _hour = DateTime.MinValue;
+        private Decimal _wage = 0;
+        private DateTime _bedTime = DateTime.MinValue;
+        private TimeSpan _midnight = new TimeSpan(24, 0, 0);
+
+        public HourlyRate(DateTime hour, DateTime bedTime)
+        {
+            this.hour = hour;
+            this.bedtime = bedtime;
+            SetWage();
+        }
+        
+        public DateTime hour { get; set; }
+
+        private DateTime bedtime { get; set; }
+
+        /// <summary>
+        /// A babyitter gets paid for the full hour.
+        /// So the wage is rounded up.
+        /// </summary>
+        public Decimal wage { get { return Math.Ceiling(_wage); } }
+
+        private void SetWage()
+        {
+            if (this.hour.TimeOfDay <= bedtime.TimeOfDay)
+            {
+                _wage = 12;
+            }
+
+            if ((this.hour.TimeOfDay < bedtime.TimeOfDay) & (this.hour.TimeOfDay >= _midnight))
+            {
+                _wage = 8;
+            }
+
+            if (this.hour.TimeOfDay > _midnight) 
+            {
+                _wage = 16;
+            }
+        }
+
+
+    }
+}
